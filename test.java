7941a98031d4e02dcc25660d03a60fd01b32cd73
kuco23 <nseverkar@gmail.com>
2020-05-28 23:45:45 +0200

    test

---
+++ test.java
@@ -0,0 +1,43 @@
+import java.util.Random;
+import java.util.ArrayList;
+
+import splosno.Koordinati;
+
+import logic.HexPlayer;
+import logic.HexLogicDfs;
+import logic.Igra;
+
+import inteligenca.Inteligenca;
+
+class test {
+    private static Random rangen = new Random();
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+
+        Igra igra = new Igra ();
+        HexLogicDfs logic = igra.getLogic();
+        Inteligenca intel = new Inteligenca();
+
+        while (true) {
+            HexPlayer player = igra.getCurrentPlayer();
+            Koordinati ij = null;
+            switch (player.index) {
+                case PLAYER0: 
+                    ArrayList<int[]> ran = logic.getEmptyFields();
+                    int[] move = ran.get(rangen.nextInt(ran.size()));
+                    ij = new Koordinati (move[0], move[1]);
+                    break;
+                case PLAYER1: 
+                    ij = intel.izberiPotezo(igra);
+                    break;
+            }
+            igra.odigraj(ij);
+            if (igra.hasWon()) break;
+        }
+        System.out.println(
+            "Winner is " + igra.getLastPlayer().index.name()
+        );
+
+        }
+    }
+}
